077e823838c54c8bbb3f88da92f393c052a07ce9
pichapat saleechanta <pichapat42@gmail.com>
2022-02-18 14:06:11 +0700

    add age and profile

--- intro-to-js/bright.js
+++ intro-to-js/bright.js
@@ -10,3 +10,21 @@ function helloWorld() {
   const h1message = document.getElementById("name-h1").innerHTML;// ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
   alert(h1message + "  ขนาดชวนยังหลีกภัย แล้วพี่เป็นใครทำไมไม่หลีกผม");
 }
+
+function calAge(){
+  const born = prompt("Year of Birth", "1999");
+  const d = new Date();
+  const year = d.getFullYear();
+  const age = year -born;
+  alert ("You stay here for " + age + " years")
+}
+
+
+function  loadProfile(){
+  const name = "BRIGHT";
+  const province = "NONTHABURI";
+
+  document.getElementById('my-name').innerHTML = name;
+  document.getElementById('my-province').innerHTML = province;
+
+}
